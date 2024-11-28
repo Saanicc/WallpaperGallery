05f1c4b73e8e4b63b84388de6cbdcc645575e987
Saanicc <mattias470@gmail.com>
2024-11-28 11:32:01 +0100

    Move styling to stylesheet

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,19 @@
 import React, { PropsWithChildren } from "react";
-import { Platform, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 const Header = ({ children }: PropsWithChildren) => {
-  return (
-    <View
-      style={{
-        width: "100%",
-        height: 50,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingHorizontal: 16,
-        top: Platform.OS === "android" ? 10 : 0,
-      }}
-    >
-      {children}
-    </View>
-  );
+  return <View style={styles.header}>{children}</View>;
 };
 
 export default Header;
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    top: Platform.OS === "android" ? 10 : 0,
+  },
+});
--- components/MenuButton/MenuButton.tsx
+++ components/MenuButton/MenuButton.tsx
@@ -1,23 +1,24 @@
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { MenuButtonProps } from "./MenuButton.config";
 
 const MenuButton = ({ icon, iconColor, onPress }: MenuButtonProps) => {
   return (
-    <TouchableOpacity
-      style={{
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        width: 50,
-        borderRadius: 50,
-      }}
-      onPress={onPress}
-    >
+    <TouchableOpacity style={styles.menuButton} onPress={onPress}>
       <Ionicons name={icon} size={30} color={iconColor} />
     </TouchableOpacity>
   );
 };
 
 export default MenuButton;
+
+const styles = StyleSheet.create({
+  menuButton: {
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    borderRadius: 50,
+  },
+});
