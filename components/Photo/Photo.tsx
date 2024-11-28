7bb16e8236146c1524450e3ebf4d4ad3bc366f65
Saanicc <mattias470@gmail.com>
2024-11-28 19:14:52 +0100

    Scale image on press

--- components/Photo/Photo.tsx
+++ components/Photo/Photo.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Image } from "react-native";
+import { Pressable } from "react-native";
 import Animated, {
   interpolate,
   useAnimatedStyle,
+  useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
 import { PhotoProps } from "./Photo.config";
 
 const Photo = ({ item, index, width, height, scrollX }: PhotoProps) => {
+  const imageCardScale = useSharedValue(1);
+
   const stylez = useAnimatedStyle(() => {
     return {
       transform: [
@@ -14,7 +18,7 @@ const Photo = ({ item, index, width, height, scrollX }: PhotoProps) => {
           scale: interpolate(
             scrollX.value,
             [index - 1, index, index + 1],
-            [0.78, 1, 0.78]
+            [0.78, imageCardScale.value, 0.78]
           ),
         },
         {
@@ -32,7 +36,20 @@ const Photo = ({ item, index, width, height, scrollX }: PhotoProps) => {
     <Animated.View
       style={[{ width, height, overflow: "hidden", borderRadius: 16 }, stylez]}
     >
-      <Image source={{ uri: item.largeImageURL }} style={{ flex: 1 }} />
+      <Pressable
+        style={{ flex: 1 }}
+        onPressIn={() => {
+          imageCardScale.value = withSpring(0.9, { mass: 0.5, damping: 0.5 });
+        }}
+        onPressOut={() => {
+          imageCardScale.value = withSpring(1, { mass: 0.5, damping: 0.5 });
+        }}
+      >
+        <Animated.Image
+          source={{ uri: item.largeImageURL }}
+          style={{ flex: 1 }}
+        />
+      </Pressable>
     </Animated.View>
   );
 };
