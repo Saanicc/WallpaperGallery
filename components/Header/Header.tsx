5bfa6ffd62eb4cf9443956f8b22b0fab6b68fdb1
Saanicc <mattias470@gmail.com>
2024-12-02 21:54:27 +0100

    Update Header component props

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,19 @@
 import React, { PropsWithChildren } from "react";
 import { Platform, StyleSheet, View } from "react-native";
 
-const Header = ({ children }: PropsWithChildren) => {
-  return <View style={styles.header}>{children}</View>;
+type HeaderProps = {
+  leftComponent?: React.ReactNode;
+  rightComponent?: React.ReactNode;
+} & PropsWithChildren;
+
+const Header = ({ leftComponent, rightComponent, children }: HeaderProps) => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.leftAction}>{leftComponent}</View>
+      <View style={styles.center}>{children}</View>
+      <View style={styles.rightAction}>{rightComponent}</View>
+    </View>
+  );
 };
 
 export default Header;
@@ -12,8 +23,22 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 50,
     flexDirection: "row",
-    justifyContent: "center",
     paddingHorizontal: 16,
     top: Platform.OS === "android" ? 10 : 0,
+  },
+  leftAction: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  center: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rightAction: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
   },
 });
