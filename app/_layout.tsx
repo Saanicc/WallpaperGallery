504a9c771c6d5bc0684858947686f80f9db7fd94
Saanicc <mattias470@gmail.com>
2024-12-04 15:14:22 +0100

    Wrap application with SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,33 +2,36 @@ import { WallpaperContextProvider } from "@/contexts/photos-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { EventProvider } from "react-native-outside-press";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <WallpaperContextProvider>
-        <EventProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              statusBarBackgroundColor: "rgba(0,0,0,0)",
-              navigationBarTranslucent: true,
-              navigationBarColor: "rgba(0,0,0,0)",
-            }}
-          >
-            <Stack.Screen name="index" />
-            <Stack.Screen
-              name="image/[id]"
-              options={{
-                animation: "fade",
-                presentation: "transparentModal",
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <WallpaperContextProvider>
+          <EventProvider>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                statusBarBackgroundColor: "rgba(0,0,0,0)",
+                navigationBarTranslucent: true,
+                navigationBarColor: "rgba(0,0,0,0)",
               }}
-            />
-          </Stack>
-        </EventProvider>
-      </WallpaperContextProvider>
-    </QueryClientProvider>
+            >
+              <Stack.Screen name="index" />
+              <Stack.Screen
+                name="image/[id]"
+                options={{
+                  animation: "fade",
+                  presentation: "transparentModal",
+                }}
+              />
+            </Stack>
+          </EventProvider>
+        </WallpaperContextProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
