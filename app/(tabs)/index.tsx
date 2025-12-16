9b804e348bf58e6c36fb7ebdd01dd0cce78ac225
Saanicc <mattias470@gmail.com>
2025-12-16 01:29:29 +0100

    Clear quick search if switching tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,11 +7,13 @@ import PixabayList from "@/components/Wallpapers/PixabayList";
 import RecentlyViewedList from "@/components/Wallpapers/RecentlyViewedList";
 import ThematicList from "@/components/Wallpapers/ThematicList";
 import { GAP } from "@/constants/style";
+import { useFilterContext } from "@/contexts/filter-context";
 import { useSettings } from "@/contexts/settings-context";
 import useTheme from "@/hooks/useTheme";
 import { PixabayImageOrder, WallpaperProvider } from "@/types/types";
 import { SCREEN_HEIGHT } from "@gorhom/bottom-sheet";
-import { useRouter } from "expo-router";
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback } from "react";
 import { Platform, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -19,6 +21,15 @@ export default function Index() {
   const { wallpaperProvider } = useSettings();
   const router = useRouter();
   const theme = useTheme();
+  const { setQuery } = useFilterContext();
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        setQuery("");
+      };
+    }, [setQuery])
+  );
 
   const navigateToWallpapers = (
     orderBy?: PixabayImageOrder,
